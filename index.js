db75dff440d6bc9c725ee246da18893b408db16e
Sean Mullen <srmullen13@gmail.com>
2018-01-09 19:27:06 -0700

    basic track writing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const parseMidi = require('midi-file').parseMidi
+const parseMidi = require('midi-file').parseMidi;
+const teoria = require('teoria');
 
 const file = fs.readFileSync('./midi/invent/invent4.mid');
 
@@ -8,7 +9,23 @@ const midi = parseMidi(file);
 // Stop execution if midi is wrong format.
 if (midi.header.format !== 1) throw new Error("Incorrect midi format. Only format 1 supported");
 
-const output = `hello from bth-scribe
-`;
+const tracks = [];
+for (let i = 1; i < midi.header.numTracks; i++) {
+    tracks.push(createTrack(`track_${i}`, midi.tracks[i]));
+}
+
+const output = tracks.reduce((str, track) => {
+    return str + track + '\n';
+}, '');
 
 fs.writeFileSync('./output.bth', output);
+
+function createTrack (name, events) {
+    notes = events.reduce((acc, event) => {
+        if (event.type === 'noteOn') {
+            return acc.concat(teoria.note.fromMIDI(event.noteNumber).scientific() + ' ');
+        }
+        return acc;
+    }, '');
+    return `[${name} ${notes}]`;
+}
