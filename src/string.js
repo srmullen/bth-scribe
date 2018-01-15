5b9d7be7d50b1b1e9bb9ec38a9c93c845ea69210
Sean Mullen <srmullen13@gmail.com>
2018-01-15 16:54:36 -0700

    write rests to bth file

--- src/string.js
+++ src/string.js
@@ -1,17 +1,25 @@
 function dotString (n) {
     if (!n) return '';
     if (n === 1) return '.';
-    if (n === 2) return '.';
+    if (n === 2) return '..';
+}
+
+function eventToString (event) {
+    if (event.type === 'rest') {
+        return `r/${event.duration.value}${dotString(event.duration.dots)}`;
+    } else {
+        return `${event.scientific()}/${event.duration.value}${dotString(event.duration.dots)}`;
+    }
 }
 
 function trackToString (track) {
     const eventString = track.events.reduce((str, event, i) => {
-        return str.concat(event.scientific() +
-        `/${event.duration.value}${dotString(event.duration.dots)}${(i+1) % 16 === 0 ? '\n' : ''} `);
+        return str.concat(eventToString(event) + `${(i+1) % 16 === 0 ? '\n' : ''} `);
     }, '');
     return `[${track.name} ${eventString}]`;
 }
 
 module.exports = {
+    eventToString,
     trackToString
 }
--- test/stringTests.js
+++ test/stringTests.js
@@ -1,8 +1,16 @@
 const {expect} = require('chai');
 const teoria = require('teoria');
-const {trackToString} = require('../src/string');
+const {eventToString, trackToString} = require('../src/string');
 
 describe('string', () => {
+    describe('eventToString', () => {
+        it('should convert rest events', () => {
+            expect(eventToString({type: 'rest', duration: {value: 4, dots: 0}})).to.equal('r/4');
+            expect(eventToString({type: 'rest', duration: {value: 16, dots: 1}})).to.equal('r/16.');
+            expect(eventToString({type: 'rest', duration: {value: 1, dots: 2}})).to.equal('r/1..');
+        });
+    });
+
     describe('trackToString', () => {
         it('converts track name to string', () => {
             expect(trackToString({name: 'track1', events: []})).to.equal('[track1 ]');
